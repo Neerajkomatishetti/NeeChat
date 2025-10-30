089f0e254d0436734c3dabe3e3ef9f623be953f3
neeraj <neerajkomatishetti@gmail.com>
2025-10-30 16:08:14 +0530

    fixed linting issues

--- app/c/[chat_id]/page.tsx
+++ app/c/[chat_id]/page.tsx
@@ -5,8 +5,11 @@ import { useParams } from "next/navigation";
 
 const Chats = () => {
   const params = useParams<{ chat_id: string }>();
-  return;
-  <Main chat_id={params.chat_id} />;
+  return (
+    <>
+      <Main chat_id={params.chat_id} />
+    </>
+  );
 };
 
 export default Chats;
